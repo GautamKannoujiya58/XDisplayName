b4cff7f8957de67c4551bdd5324b3c700c8801da
gautamKumar <gautamkannoujiya58@gmail.com>
2024-09-05 18:40:10 +0530

    Resolving the test cases for empty field submission

--- xdisplayname/src/DisplyFullName.jsx
+++ xdisplayname/src/DisplyFullName.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 function DisplayFullName() {
   const [fullName, setFullName] = useState(null);
-
+  useEffect(() => {}, []);
   const handleSubmit = (e) => {
     console.log("Submit clicked");
     e.preventDefault();
@@ -11,20 +11,32 @@ function DisplayFullName() {
     console.log("firstName >>>", firstName, "lastName >>>", lastName);
     setFullName(firstName + " " + lastName);
   };
-
+  console.log("Submit clicked");
   console.log("fullName >>>", fullName);
-
+  console.log("Submit clicked");
   return (
     <>
       <h1>Full Name Display</h1>
       <form action="/submit-form" method="POST" onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name:</label>
-        <input required type="text" id="firstName" name="firstName" />
+        <input
+          //   onChange={handlefirstNameChange}
+          required
+          type="text"
+          id="firstName"
+          name="firstName"
+        />
         <br />
         <label htmlFor="lastName">Last Name:</label>
-        <input required type="text" id="lastName" name="lastName" />
+        <input
+          //   onChange={handleSecondNameChange}
+          required
+          type="text"
+          id="lastName"
+          name="lastName"
+        />
         <br />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
       <h3>Full Name: {fullName}</h3>
     </>
