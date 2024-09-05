9f2af5829f7ee963c3a2e45a47bd2ba532b9edcb
gautamKumar <gautamkannoujiya58@gmail.com>
2024-09-05 19:25:58 +0530

    Test case for submitting with empty input field

--- xdisplayname/src/DisplyFullName.jsx
+++ xdisplayname/src/DisplyFullName.jsx
@@ -8,37 +8,26 @@ function DisplayFullName() {
     console.log("Form fields >>>", e);
     const firstName = e.target.elements.firstName.value;
     const lastName = e.target.elements.lastName.value;
-    console.log("firstName >>>", firstName, "lastName >>>", lastName);
+    // console.log("firstName >>>", firstName, "lastName >>>", lastName);
     setFullName(firstName + " " + lastName);
   };
-  console.log("Submit clicked");
-  console.log("fullName >>>", fullName);
+  //   console.log("Submit clicked");
+  //   console.log("fullName >>>", fullName);
   console.log("Submit clicked");
   return (
     <>
       <h1>Full Name Display</h1>
       <form action="/submit-form" method="POST" onSubmit={handleSubmit}>
         <label htmlFor="firstName">First Name:</label>
-        <input
-          //   onChange={handlefirstNameChange}
-          required
-          type="text"
-          id="firstName"
-          name="firstName"
-        />
+        <input required type="text" id="firstName" name="firstName" />
         <br />
         <label htmlFor="lastName">Last Name:</label>
-        <input
-          //   onChange={handleSecondNameChange}
-          required
-          type="text"
-          id="lastName"
-          name="lastName"
-        />
+        <input required type="text" id="lastName" name="lastName" />
         <br />
         <button type="submit">Submit</button>
       </form>
-      <h3>Full Name: {fullName}</h3>
+      {fullName && <h3>Full Name: {fullName}</h3>}
+      {/* This can also be code as {fullName ? <h3>Full Name: {fullName}: null} */}
     </>
   );
 }
